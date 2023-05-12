4fb48c663a2c29924365184f0c1c889c60793466
Adam1XD <adam-blha@email.cz>
2023-05-12 10:29:57 +0200

    Add project files.

---
+++ DowloandSpeed/DFile.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DowloandSpeed
+{
+    public class DFile
+    {
+        private double Gygabytes { get; set; }
+        private double Megabytes { get; set; }
+
+        private double FullSize()
+        {
+            return Gygabytes * 1000 + Megabytes;
+        }
+
+        public string DowloandSpeed(double dowloandSpeed)
+        {
+            double totalSeconds = FullSize() / dowloandSpeed;
+            int hours = (int)totalSeconds / 3600;
+            int minutes = (int)(totalSeconds % 3600) / 60;
+            int seconds = (int)(totalSeconds % 60);
+            return $"Your file will be downloaded in: {hours} hours - {minutes} minutes - {seconds} seconds.";
+        }
+
+        public void Choice()
+        {
+            bool error = false;
+            do
+            { 
+                Console.WriteLine("Choose your Insert type:\n1 gygabytes\n2 megabytes\n3 both");
+                char choice = char.Parse(Console.ReadLine());
+                switch(choice)
+                {
+                    case '1':
+                        Console.WriteLine("Insert the file size in gigabytes: ");
+                        Gygabytes = double.Parse(Console.ReadLine());
+                        break;
+                    case '2':
+                        Console.WriteLine("Insert the file size in megabytes: ");
+                        Megabytes = double.Parse(Console.ReadLine());
+                        break;
+                    case '3':
+                        Console.WriteLine("Insert the file size in gigabytes: ");
+                        Gygabytes = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Insert the file size in megabytes: ");
+                        Megabytes = double.Parse(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("Error. Try again.");
+                        error = true;
+                        break;
+            
+                }
+            }while(error == true);
+        }
+    }
+}
---
+++ DowloandSpeed/Program.cs
@@ -0,0 +1,7 @@
+﻿using DowloandSpeed;
+
+Console.WriteLine("---Dowland speed Calculator---");
+DFile file = new();
+file.Choice();
+Console.WriteLine("Inser your dowloand speed in megabytes:");
+Console.WriteLine(file.DowloandSpeed(double.Parse(Console.ReadLine()))); 
